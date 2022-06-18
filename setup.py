213e2a9c89b6b2d1abf29b685d77280d346d6b63
Aashish Pokharel <pokharellaashish454@gmail.com>
2022-06-18 20:41:26 +0545

    [First] Initialized the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A sentiment analysis project based on Sentiment140 dataset to recognize the emotions(happy/sad) from the given tweets',
+    author='Aashish Pokharel',
+    license='BSD-3',
+)
